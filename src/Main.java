77ea1cce12f5d54314b23cdc3ef287704178d8df
Jules <jules.dempt@outlook.fr>
2021-10-11 11:31:10 +0200

    test
    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 import com.formdev.flatlaf.FlatLightLaf;
 import controller.App;
-import controller.GameController;
 
 public class Main {
     public static void main(String[] args) {
         FlatLightLaf.setup();
 
         App app = App.getInstance();
-
-        /*GameController gameManager = GameController.getInstance();
-
-        GameFrame frameManager = new GameFrame();
-        frameManager.setVisible(true);*/
-
     }
 }
